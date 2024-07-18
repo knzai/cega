a543be5ca3753aecf26725fff487f77c3d00e329
Kenzi Connor <knzconnor@gmail.com>
2024-07-18 11:07:15 -0700

    unfactor a bit pre reorg

--- src/wasm/main.rs
+++ src/wasm/main.rs
@@ -23,7 +23,7 @@ struct FileDetails {
 
 pub enum Msg {
     Loaded(String, String, Vec<u8>),
-    Files(Vec<File>),
+    Files(Option<FileList>),
 }
 
 pub struct App {
@@ -54,23 +54,30 @@ impl Component for App {
                 true
             }
             Msg::Files(files) => {
-                for file in files.into_iter() {
-                    let file_name = file.name();
-                    let file_type = file.raw_mime_type();
+                if let Some(files) = files {
+                    for file in js_sys::try_iter(&files)
+                        .unwrap()
+                        .unwrap()
+                        .map(|v| web_sys::File::from(v.unwrap()))
+                        .map(File::from)
+                    {
+                        let file_name = file.name();
+                        let file_type = file.raw_mime_type();
 
-                    let task = {
-                        let link = ctx.link().clone();
-                        let file_name = file_name.clone();
+                        let task = {
+                            let link = ctx.link().clone();
+                            let file_name = file_name.clone();
 
-                        gloo::file::callbacks::read_as_bytes(&file, move |res| {
-                            link.send_message(Msg::Loaded(
-                                file_name,
-                                file_type,
-                                res.expect("failed to read file"),
-                            ))
-                        })
-                    };
-                    self.readers.insert(file_name, task);
+                            gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                                link.send_message(Msg::Loaded(
+                                    file_name,
+                                    file_type,
+                                    res.expect("failed to read file"),
+                                ))
+                            })
+                        };
+                        self.readers.insert(file_name, task);
+                    }
                 }
                 true
             }
@@ -82,23 +89,6 @@ impl Component for App {
             <div id="wrapper">
                 <p id="title">{ "Process your image files" }</p>
                 <label for="file-upload">
-                    <div
-                        id="drop-container"
-                        ondrop={ctx.link().callback(|event: DragEvent| {
-                            event.prevent_default();
-                            let files = event.data_transfer().unwrap().files();
-                            Self::upload_files(files)
-                        })}
-                        ondragover={Callback::from(|event: DragEvent| {
-                            event.prevent_default();
-                        })}
-                        ondragenter={Callback::from(|event: DragEvent| {
-                            event.prevent_default();
-                        })}
-                    >
-                        <i class="fa fa-cloud-upload"></i>
-                        <p>{"Drop your images here or click to select"}</p>
-                    </div>
                 </label>
                 <input
                     id="file-upload"
@@ -107,7 +97,7 @@ impl Component for App {
                     multiple={true}
                     onchange={ctx.link().callback(move |e: Event| {
                         let input: HtmlInputElement = e.target_unchecked_into();
-                        Self::upload_files(input.files())
+                        Msg::Files(input.files())
                     })}
                 />
                 <div id="preview-area">
@@ -142,20 +132,6 @@ impl App {
                 </div>
             </div>
         }
-    }
-
-    fn upload_files(files: Option<FileList>) -> Msg {
-        let mut result = Vec::new();
-
-        if let Some(files) = files {
-            let files = js_sys::try_iter(&files)
-                .unwrap()
-                .unwrap()
-                .map(|v| web_sys::File::from(v.unwrap()))
-                .map(File::from);
-            result.extend(files);
-        }
-        Msg::Files(result)
     }
 }
 
